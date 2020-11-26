ca832ee4cb6f6dde198fa197991ffcc71a00b0da
skiedflakes <gagito0594@gmail.com>
2020-11-26 16:50:51 +0800

    jr
    jr

--- consumables/get_table_data.php
+++ consumables/get_table_data.php
@@ -8,13 +8,12 @@ $start_date = $_REQUEST['start_date'];
 $end_date = $_REQUEST['end_date'];
 set_database($company_code);
 
-
-$sales_data = mysql_query("SELECT * from tbl_consumbales_header  where company_id = '$company_id' and branch_id = '$branch_id' AND (date_added >= '$start_date' and date_added <= '$end_date') order by date_added DESC");
+$fetch_table_data = mysql_query("SELECT * from tbl_consumables_header where company_id = '$company_id' and branch_id = '$branch_id' AND (date_added >= '$start_date' and date_added <= '$end_date') order by date_added DESC");
 
 // $sales_data =mysql_query("SELECT * FROM `tbl_dr_header` where dr_date ='2020-09-17' order by dr_date DESC");
 	$response_array['array_data'] = array();
-	while ($data = mysql_fetch_array($sales_data)) {
-        $response["consumbales_header_id"] = $data["consumbales_header_id"];
+	while ($data = mysql_fetch_array($fetch_table_data)) {
+        $response["consumables_header_id"] = $data["consumables_header_id"];
 		$response["consumable_number"] = $data["consumable_number"];
 		array_push($response_array['array_data'], $response);
 	}
